8c10ea0584691f3ccc4a3663ead2ffb404ef905e
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2022-04-13 22:46:43 +0900

    Add interactive demo counter

--- teinei-text-counter-demo/next.config.js
+++ teinei-text-counter-demo/next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = module.exports = {
+  ...nextConfig,
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+    config.output.webassemblyModuleFilename = (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
+    return config;
+  },
+};
