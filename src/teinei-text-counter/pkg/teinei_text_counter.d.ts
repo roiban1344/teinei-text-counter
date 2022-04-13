74b9c7de549d72f43f83dd0d26b795aeeb0229f2
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2022-04-14 00:02:32 +0900

    Update .gitignore to track type declaration file

---
+++ src/teinei-text-counter/pkg/teinei_text_counter.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} s
+* @returns {number}
+*/
+export function countGraphemeClusters(s: string): number;
+/**
+* @param {string} s
+* @returns {Array<string>}
+*/
+export function splitIntoGraphemeClusters(s: string): Array<string>;
